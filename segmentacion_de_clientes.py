463e76f3f76bbcbb321a4b23c00b01fa93720325
MerNicosia <105322292+MerNicosia@users.noreply.github.com>
2022-07-17 23:58:47 -0300

    Update segmentacion_de_clientes.py

--- segmentacion_de_clientes.py
+++ segmentacion_de_clientes.py
@@ -54,7 +54,81 @@ for i in user.transacciones:
     user.alta_chequera(i)
     user.compra_dolar(i)
 
+columna=''
+for transaccion in user.transacciones:
+    columna+= '<tr>'
+    for cadatributo in user.transacciones():
+        columna+= f'<td> {cadatributo} </td>'
+    columna += '</td>'
 
+import codecs
+
+f =open ('archivo.html', 'w','utf-8')
+    
+html_template= """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+    crossorigin="anonymous"/>
+    <title>Reporte Transacciones</title>
+</head>
+<body style="background-color:rgba(182, 177, 182, 0.50)">
+    <div class="container" >
+        <div class="card" style="background-color: rgba(173, 216, 230, 0.3);">
+            <div class="row justify-content-center" id="titulo" style="font-size: 30px; padding: 30px;">Reporte de Transacciones</div>
+            <div class="row">
+                <div class="col-md-12 mx-auto" style="padding: 30px;">
+                    <div class="card-header">
+                        <h1 style="font-size: 20px">Cliente {variable cliente, clase y nombre y apellido}</h1>
+                    </div>
+                    <ul class="list-group">
+                        <li class="list-group-item"> Numero de Cuenta: {num de cuenta} </li>
+                        <li class="list-group-item"> DNI: {num de dni} </li>
+                        <li class="list-group-item"> Direccion: {direccion} </li>
+                    </ul>
+                    <table class="table table-responsive">
+                        <thead>
+                        <tr>
+                            <th scope="col">Numero</th>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">Monto</th>
+                            <th scope="col">Razon</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            {columna}
+                        </tr>
+                            
+                            
+                        </tbody>
+                    </table>
+                </div>
+                    
+                            
+            </div>
+
+        </div>
+
+    /div>
+
+</body>
+</html>"""
+
+f.write(html_template)
+
+f.close()
+
+file = codecs.open("archivo.html", 'r', "utf-8")
+print(file.read())
 
 
 
